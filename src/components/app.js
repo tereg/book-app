a7eb363b5f63aa2a9eec62d58aa8b15e5f8cb6b9
tereg <teregalla@gmail.com>
2017-01-16 12:03:56 -0800

    Rename book-list

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
 
-import BookList from '../containers/book_list';
+import BookList from '../containers/book-list';
 import BookDetail from '../containers/book-detail';
 
 export default class App extends Component {
